3036bcb324ea7c7ae40951816eee0540ea8eab93
nieli <>
2021-04-11 14:33:32 +0800

    fix 页面二11

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import { withLoadable } from "./src/utils";
 const App = withLoadable(() => import("./src/component/App/AppView"));
 // const Hooks = withLoadable(() => import("./src/views/hooks"));
 const About = () => <h2>页面一</h2>;
-const Users = () => <h2>页面二</h2>;
+const Users = () => <h2>页面二11</h2>;
 ReactDom.render(
   <Provider store={store}>
     <Router>
